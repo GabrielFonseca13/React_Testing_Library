bb7393b35b0ea8e61db77a08db84d7ae834916c6
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-11-05 20:59:54 -0300

    resolução do requisito 6.1

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes do Requisito 6', () => {
+  it('Teste se é renderizado um card com as informações de determinado pokémon: nome, tipo, peso, imagem', () => {
+    renderWithRouter(<App />);
+    const pokemonList = pokemons;
+    const { averageWeight } = pokemonList[0];
+
+    const pokemonName = screen.getByTestId(/pokemon-name/i);
+    expect(pokemonName).toHaveTextContent(pokemonList[0].name);
+
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent(pokemonList[0].type);
+
+    const pokemonWeight = screen.getByTestId(/pokemon-weight/i);
+    expect(pokemonWeight)
+      .toHaveTextContent(`${averageWeight.value} ${averageWeight.measurementUnit}`);
+
+    const imgPokemon = screen.getByRole('img');
+    expect(imgPokemon).toHaveAttribute('src', pokemonList[0].image);
+    expect(imgPokemon).toHaveAttribute('alt', `${pokemonList[0].name} sprite`);
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// // import { act } from 'react-dom/test-utils';
+// import renderWithRouter from '../renderWithRouter';
+// import App from '../App';
+
+// describe('Testes do Requisito 7', () => {
+//   it('', () => {
+
+//   });
+// });
