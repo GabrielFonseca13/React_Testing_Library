fc33e381cd5ace5dac0217c4a0a01c8330f77e38
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-11-05 17:39:22 -0300

    resolução do requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('Teste 1', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+// import App from '../App';
+import { About } from '../pages';
+
+describe('Testes do Requisito 2', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutTitle = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(aboutTitle).toBeInTheDocument();
+
+    const paragraph1 = screen.getByText(/This application simulates a Pokédex, a digital encyclopedia containing all Pokémons/i);
+    expect(paragraph1).toBeInTheDocument();
+    const paragraph2 = screen.getByText(/One can filter Pokémons by type, and see more details for each one of them/i);
+    expect(paragraph2).toBeInTheDocument();
+
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
