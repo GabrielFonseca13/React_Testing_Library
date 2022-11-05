d87932995ff21d375890bffcb58d98614d871072
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-11-05 18:17:14 -0300

    resolução do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes do Requisito 4', () => {
+  it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+    const INVALID_URL = '/pagina-inexistente';
+    act(() => {
+      history.push(INVALID_URL);
+    });
+
+    const notFoundTitle = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(notFoundTitle).toBeInTheDocument();
+
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
